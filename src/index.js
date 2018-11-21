8b63fec1837ae0e1ae5386a9c32a81b08322c015
xiangcheng0618 <917584516@qq.com>
2018-11-21 10:49:22 +0800

    v0.0.1 初次

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert('LaserCache')
+class LaserCache {
+    constructor(){
+        this.list ={}  //空对象
+    }
+    set(key, value) {
+        this.list[key] =value //设置一个值
+    }
+    get (key) {
+        return this.list[key] //获取一个值
+    }
+}
+
+window.LaserCache =LaserCache
